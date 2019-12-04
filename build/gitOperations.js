4a0b4b626eef2e2e7215af6aca4061629b25e77e
logan70 <im.lizhh@gmail.com>
2019-12-04 09:18:15 +0800

    chore: use 'git commit' instead of 'git ci' alias

--- build/gitOperations.js
+++ build/gitOperations.js
@@ -4,7 +4,7 @@ const { execSync } = require('child_process')
 exports.gitPush = function gitPush(msg = 'add: add blog') {
   const commands = [
     'git add -A',
-    `git ci -m '${msg}'`,
+    `git commit -m '${msg}'`,
     'git push -f',
   ]
   commands.forEach(cmd => execSync(cmd))
